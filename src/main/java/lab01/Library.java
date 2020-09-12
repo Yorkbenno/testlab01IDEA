f0d51270f0ac4baf68aee93715a65a6d7942ffe0
Yorkbenno <1007368294@qq.com>
2020-09-12 23:00:24 +0800

    initial

---
+++ src/main/java/lab01/Library.java
@@ -0,0 +1,11 @@
+package lab01;
+
+public class Library {
+    public static void main(String[] args) {
+        System.out.println("When there is a fire, commit and push.");
+    }
+
+    public boolean someLibraryMethods(){
+        return true;
+    }
+}
